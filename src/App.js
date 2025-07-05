2a858ec95b2c949f8c2670f1aa85ad7457a3597b
Deepak Waghmare <deepakwaghmare110@gmail.com>
2025-07-06 01:24:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
 import Form from "./components/Form";
+import ToDoList from "./components/ToDoList";
 const App = () => {
   return (
     <div>
       <header className="heading">Deeps To Do List</header>
       <Form />
+      <ToDoList />
     </div>
   );
 };
---
+++ src/components/ToDoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ToDoList = () => {
+  return (
+    <div>
+      <ul className="todo-list"></ul>
+    </div>
+  );
+};
+
+export default ToDoList;
