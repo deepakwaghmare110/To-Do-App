e00194aecf974a056db9495c71bf97e12c14f138
Deepak Waghmare <deepakwaghmare110@gmail.com>
2025-07-09 21:07:04 +0530

    state added

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const ToDoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [updateTasks, setUpdateTasks] = useState("");
+
+  function handleInputChange(e) {
+    setTasks(e.target.value);
+  }
+
   return (
     <div>
       <h1> Deepak's To Do List</h1>
+      <input
+        type="text"
+        value={tasks}
+        onChange={handleInputChange}
+        placeholder="Enter a task"
+      />
     </div>
   );
 };
